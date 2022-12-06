ad7e9592e07b119a2671c8b7f5bc7b38d8a837ea
omondidenzel <akwanydenzel@gmail.com>
2022-12-06 07:06:44 +0300

    added model

--- website/rfClassifier/views.py
+++ website/rfClassifier/views.py
@@ -4,16 +4,22 @@ import joblib
 
 # Create your views here.
 def index(request):
-    num_preg = float(request.GET["num_preg"])
-    glucose_conc = float(request.GET["glucose_conc"])
-    diastolic_bp = float(request.GET["diastolic_bp"])
-    thickness = float(request.GET["thickness"])
-    insulin = float(request.GET["insulin"])
-    bmi = float(request.GET["bmi"])
-    diab_pred = float(request.GET["diab_pred"])
-    age = float(request.GET["age"])
-    skin = float(request.GET["skin"])
+    return render(request, "index.html")
 
-    usingJoblib = joblib.load("diabetes_rf_model")
-    result = usingJoblib.predict([[num_preg, glucose_conc, diastolic_bp, thickness, insulin, bmi, diab_pred, age, skin]])
-    return render(request, "index.html", {result: "output"})
+def result(request):
+    num_preg = float(request.GET["num_preg1"])
+    glucose_conc = float(request.GET["glucose_conc1"])
+    diastolic_bp = float(request.GET["diastolic_bp1"])
+    thickness = float(request.GET["thickness1"])
+    insulin = float(request.GET["insulin1"])
+    bmi = float(request.GET["bmi1"])
+    diab_pred = float(request.GET["diab_pred1"])
+    age = float(request.GET["age1"])
+    skin = float(request.GET["skin1"])
+
+    with open("/Users/omondidenzel/Desktop/DIABETES-ROJECT/website/rfClassifier/diabetes_rf_model.pkl", "rb") as f:
+        usingJoblib = joblib.load(f)
+        result = usingJoblib.predict([[num_preg, glucose_conc, diastolic_bp, thickness, insulin, bmi, diab_pred, age, skin]])
+    #Testing purpose
+    #result =10000
+    return render(request, "result.html", {"result": result})
